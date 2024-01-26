acad0edd4657ce1c0519b2d09781922f52e22bed
khaletska <khaletska.e@gmail.com>
2024-01-26 20:08:58 +0200

    aninations added to experience and projects

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,61 +1,84 @@
 import './experience.css';
 
-const Experience = () => {
-    return (
-        <section className="container">
-            <div className="wrapper">
-                <h2>Experience</h2>
-                <ul className="list">
-                    <li className="list-item">
-                        <div className='list-first-line'>
-                            <p className="date">2023.09 - 2023.12</p>
-                            <p className="position">Front-End Developer</p>
-                        </div>
+const Experience = () => (
+    <section className="container">
+        <div className="wrapper">
+            <h2>Experience</h2>
+            <ul className="exp-list">
+                <li className="exp-list-item">
+                    <div className='exp-list-first-line'>
+                        <p className="exp-date">2023.09 - 2023.12</p>
+                        <p className="position">Front-End Developer</p>
+                    </div>
+                    <div className='company-box'>
+                        <a
+                            className='company'
+                            href='https://minds.ee/'
+                            target='_blank'
+                            title='Open in new tab'
+                            rel="noreferrer"
+                        >MindSee</a>
+                    </div>
+                </li>
+                <li className="exp-list-item">
+                    <div className='exp-list-first-line'>
+                        <p className="exp-date">2023.05 - 2023.07</p>
+                        <p className="position">iOS Intern</p>
+                    </div>
 
-                        <a href='https://minds.ee/' target='_blank' title='Open in new tab' rel="noreferrer" className="company">MindSee</a>
-                        {/* <p className="work-desc">Bla-bla-bla experience skills stuff</p> */}
-                    </li>
-                    <li className="list-item">
-                        <div className='list-first-line'>
-                            <p className="date">2023.05 - 2023.07</p>
-                            <p className="position">iOS Intern</p>
-                        </div>
+                    <div className='company-box'>
+                        <a
+                            className='company'
+                            href='https://proekspert.com/'
+                            target='_blank'
+                            title='Open in new tab'
+                            rel="noreferrer"
+                        >Proekspert AS</a>
+                    </div>
+                </li>
+                <li className="exp-list-item">
+                    <div className='exp-list-first-line'>
+                        <p className="exp-date">2021.12 - 2022.07</p>
+                        <p className="position">Design Engineer</p>
+                    </div>
+                    <div className='company-box'>
+                        <a
+                            className='company'
+                            href='https://urartu.com/'
+                            target='_blank'
+                            title='Open in new tab'
+                            rel="noreferrer"
+                        >Urartu Systems</a>
+                    </div>
+                </li>
+                <li className="exp-list-item">
+                    <div className='exp-list-first-line'>
+                        <p className="exp-date">2019.01 - 2021.12</p>
+                        <p className="position">Engineer</p>
+                    </div>
+                    <div className='company-box'>
+                        <a
+                            className='company'
+                            href='https://cps63.ru/en/'
+                            target='_blank'
+                            title='Open in new tab'
+                            rel="noreferrer"
+                        >CPS technology</a>
+                    </div>
+                </li>
+                <li className="exp-list-item">
+                    <div className='exp-list-first-line'>
+                        <p className="exp-date">2016.10 - 2018.07</p>
+                        <p className="position">Product Manager</p>
+                    </div>
 
-                        <a href='https://proekspert.com/' target='_blank' title='Open in new tab' rel="noreferrer" className="company">Proekspert AS</a>
-                        {/* <p className="work-desc">Bla-bla-bla experience skills stuff</p> */}
-                    </li>
-                    <li className="list-item">
-                        <div className='list-first-line'>
-
-                            <p className="date">2021.12 - 2022.07</p>
-                            <p className="position">Design Engineer</p>
-                        </div>
-
-                        <a href='https://urartu.com/' target='_blank' title='Open in new tab' rel="noreferrer" className="company">Urartu Systems</a>
-                        {/* <p className="work-desc">Bla-bla-bla experience skills stuff</p> */}
-                    </li>
-                    <li className="list-item">
-                        <div className='list-first-line'>
-                            <p className="date">2019.01 - 2021.12</p>
-                            <p className="position">Engineer</p>
-                        </div>
-
-                        <a href='https://cps63.ru/en/' target='_blank' title='Open in new tab' rel="noreferrer" className="company">CPS technology</a>
-                        {/* <p className="work-desc">Bla-bla-bla experience skills stuff</p> */}
-                    </li>
-                    <li className="list-item">
-                        <div className='list-first-line'>
-                            <p className="date">2016.10 - 2018.07</p>
-                            <p className="position">Product Manager</p>
-                        </div>
-
+                    <div className='company-box'>
                         <p className="company">Prof-press</p>
-                        {/* <p className="work-desc">Bla-bla-bla experience skills stuff</p> */}
-                    </li>
-                </ul>
-            </div>
-        </section>
-    )
-}
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+)
 
 export default Experience;
--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -3,31 +3,70 @@ import './projects.css';
 const Projects = () => {
     return (
         <section className="container">
-            <div style={{ width: "calc(100% - 2rem)" }} className="wrapper">
+            <div
+                style={{ width: "100%" }}
+                className="wrapper">
                 <h2>Projects</h2>
                 <div className="proj-list">
-                    <a className="proj-item" href="https://github.com/khaletska/SORK">
-                        <p className="proj-name">SORK</p>
+                    <div className="proj-item">
 
-                        <img src="https://media.discordapp.net/attachments/1075493710692876330/1199429701396078774/Screenshot_from_2024-01-23_21-04-47.png?ex=65c282f5&is=65b00df5&hm=24d22a153620e66d6f8e8630bb0246cdaa9a2ae6a0a767907afd7a58ae15900d&=&format=webp&quality=lossless&width=627&height=489"
-                            alt="SORK" />
-                    </a>
-                    <a className="proj-item" href="https://github.com/khaletska/tetris">
-                        <p className="proj-name">tetris</p>
-
-                        <img src="https://media.discordapp.net/attachments/1075493710692876330/1199431913174212679/Screenshot_from_2024-01-23_21-14-00.png?ex=65c28504&is=65b01004&hm=8446fe1a37aad02b931555f301fdf317921c6c843c178ccd3c3200ef4dcc693c&=&format=webp&quality=lossless&width=632&height=489"
-                            alt="tetris" />
-                    </a>
-                    <a className="proj-item" href="https://github.com/khaletska/foorum">
-                        <p className="proj-name">forum</p>
-
-                        <img src="https://media.discordapp.net/attachments/1075493710692876330/1199431351187820585/Screenshot_from_2024-01-23_21-11-49.png?ex=65c2847e&is=65b00f7e&hm=72d15cddd49db0ca5bbec843259a0c8bf547ef31bd113edea55f50b809152c38&=&format=webp&quality=lossless&width=632&height=489"
-                            alt="forum" />
-                    </a>
-
+                        <div className='front'>
+                            <p className="proj-name">SORK</p>
+                            <p className='proj-desc'>
+                                A single-page Facebook-like application created with React.js, Typescript, SCSS for front-
+                                end and Golang, Sqlite3 for backend API server.
+                            </p>
+                        </div>
+                        <div className='back'>
+                            <img src="https://media.discordapp.net/attachments/1075493710692876330/1199429701396078774/Screenshot_from_2024-01-23_21-04-47.png?ex=65c282f5&is=65b00df5&hm=24d22a153620e66d6f8e8630bb0246cdaa9a2ae6a0a767907afd7a58ae15900d&=&format=webp&quality=lossless&width=627&height=489"
+                                alt="SORK" />
+                            <div className='btn-box'>
+                                <a className='btn' href="https://github.com/khaletska/SORK">
+                                    Repo
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="proj-item">
+                        <div className='front'>
+                            <p className="proj-name">tetris</p>
+                            <p className='proj-desc'>
+                                Single player Tetris game developed using vanilla JavaScript with DOM manipulation,
+                                without the need for HTML canvas or any frameworks. It utilizes a
+                                Golang web server and Gorilla websocket as the backend to display scores.
+                            </p>
+                        </div>
+                        <div className='back'>
+                            <img src="https://media.discordapp.net/attachments/1075493710692876330/1199431913174212679/Screenshot_from_2024-01-23_21-14-00.png?ex=65c28504&is=65b01004&hm=8446fe1a37aad02b931555f301fdf317921c6c843c178ccd3c3200ef4dcc693c&=&format=webp&quality=lossless&width=632&height=489"
+                                alt="tetris" />
+                            <div className='btn-box'>
+                                <a className='btn' href="https://github.com/khaletska/tetris">
+                                    Repo
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="proj-item">
+                        <div className='front'>
+                            <p className="proj-name">forum</p>
+                            <p className='proj-desc'>
+                                A Golang-based web forum, incorporating user authentication with encrypted passwords
+                                in an SQLite database, Docker for containerization.
+                            </p>
+                        </div>
+                        <div className='back'>
+                            <img src="https://media.discordapp.net/attachments/1075493710692876330/1199431351187820585/Screenshot_from_2024-01-23_21-11-49.png?ex=65c2847e&is=65b00f7e&hm=72d15cddd49db0ca5bbec843259a0c8bf547ef31bd113edea55f50b809152c38&=&format=webp&quality=lossless&width=632&height=489"
+                                alt="forum" />
+                            <div className='btn-box'>
+                                <a className='btn' href="https://github.com/khaletska/foorum">
+                                    Repo
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
