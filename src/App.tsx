2225471ff15a6b1a92f7532cab0128582c6dc7ed
khaletska <khaletska.e@gmail.com>
2024-02-21 21:09:14 +0200

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/portfolio" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
