2c1b9fca01f712ae8fda2d84e3d3e3b5ccd47e25
Elena Khaletska <yeliena.khaletska@pipedrive.com>
2024-07-18 21:23:52 +0300

    Update data

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -5,10 +5,10 @@ const Welcome = () => {
         <section style={{ minHeight: "calc(100vh - 109px)" }} className="container">
             <div id="about" className="wrapper">
                 <h1>Elena Khaletska</h1>
-                <p className="intro">Former Front-end Developer at MindSee and iOS Intern at Proekspert AS, currently advancing my skills at kood/Jõhvi School. I am passionate about turning stunning designs into seamless user experiences. With a technical mind and over 5 years of experience in various engineering roles, I am equally able to work independently and as a collaborative team player.
+                <p className="intro">iOS intern at Pipedrive, kood/Jõhvi student. I am passionate about turning stunning designs into seamless user experiences. With a technical mind and over 5 years of experience in various engineering roles, I am equally able to work independently and as a collaborative team player.
                     <br />
                     <br />
-                    My favorite tools include, but not limited by React, TypeScript, Swift and Golang. I am always ready to dive into new technologies to ensure the best outcomes for all. Feel free to explore the page, and don't hesitate to reach out with any questions or just for a friendly chat.</p>
+                    My favorite tools include, but not limited by Swift, React, TypeScript, and Golang. I am always ready to dive into new technologies to ensure the best outcomes for all. Feel free to explore the page, and don't hesitate to reach out with any questions or just for a friendly chat.</p>
             </div>
         </section>
     )
